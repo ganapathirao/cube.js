89250d92b45f05658883b927ff3f091152598bd9
Dmitry Patsura <zaets28rus@gmail.com>
2021-02-16 12:09:29 +0300

    tmp

--- .github/actions/cross-ubuntu-musl.sh
+++ .github/actions/cross-ubuntu-musl.sh
@@ -7,7 +7,7 @@ sudo apt-get -y install musl-tools llvm
 
 wget https://www.openssl.org/source/openssl-1.1.1i.tar.gz -O - | tar -xz &&\
   cd openssl-1.1.1i && \
-  CC="musl-gcc -fPIE -pie" ./Configure no-shared no-async --prefix=/musl --openssldir=/musl/ssl linux-x86_64 && \
+  CC="musl-gcc -fPIE -pie" ./Configure no-shared no-async no-engine -DOPENSSL_NO_SECURE_MEMORY --prefix=/musl --openssldir=/musl/ssl linux-x86_64 && \
   make depend && \
   make -j $(nproc) && \
   sudo make install && \
